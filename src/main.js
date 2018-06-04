ef07af37048dcd8b5674adcc7dc7f498d16df666
Brandon <brandoncyu@gmail.com>
2018-06-04 16:17:03 -0700

    added current color indicator

--- src/main.js
+++ src/main.js
@@ -9,57 +9,63 @@ let indigo = document.querySelector('#indigo')
 let violet = document.querySelector('#violet')
 let black = document.querySelector('#black')
 let white = document.querySelector('#white')
-let erase = document.querySelector('#erase')
+let currentColor = document.querySelector('#current-color')
 
 red.addEventListener('click', function(){
   selected = 'red'
   console.log(selected)
+  currentColor.style.backgroundColor=selected
 })
 
 orange.addEventListener('click', function(){
   selected = 'orange'
   console.log(selected)
+  currentColor.style.backgroundColor=selected
 })
 
 yellow.addEventListener('click', function(){
   selected = 'yellow'
   console.log(selected)
+  currentColor.style.backgroundColor=selected
 })
 
 green.addEventListener('click', function(){
   selected = 'green'
   console.log(selected)
+  currentColor.style.backgroundColor=selected
 })
 
 blue.addEventListener('click', function(){
   selected = 'blue'
   console.log(selected)
+  currentColor.style.backgroundColor=selected
 })
 
 indigo.addEventListener('click', function(){
   selected = 'indigo'
   console.log(selected)
+  currentColor.style.backgroundColor=selected
 })
 
 violet.addEventListener('click', function(){
   selected = 'violet'
   console.log(selected)
+  currentColor.style.backgroundColor=selected
 })
 
 black.addEventListener('click', function(){
   selected = 'black'
   console.log(selected)
+  currentColor.style.backgroundColor=selected
 })
 
 white.addEventListener('click', function(){
   selected = 'white'
   console.log(selected)
+  currentColor.style.backgroundColor=selected
 })
 
-erase.addEventListener('click', function(){
-  selected = ''
-  console.log(selected)
-})
+
 
 
 grid = Array.from(document.querySelectorAll('#grid td'))
